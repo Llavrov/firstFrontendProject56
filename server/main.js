f2c98411431974f69089bf650212fcf3fe777ee2
leva <levlevlavrov@gmail.com>
2021-11-13 20:22:36 +0500

    connect to public

--- server/main.js
+++ server/main.js
@@ -6,15 +6,13 @@ const fs = require('fs')
 const server = http.createServer((request, response) => {
     console.log(request.method, request.url);
     if (request.url == '/style.css') {
-        const css = fs.readFileSync('style.css', 'utf8');
+        const css = fs.readFileSync('../public/style.css', 'utf8');
         response.end(css);
     } else {
-        const html = fs.readFileSync('index.html', 'utf8');
+        const html = fs.readFileSync('../public/index.html', 'utf8');
         response.end(html);
     }
 });
 
-const PORT = process.env.PORT  || 3000;
-
-server.listen(PORT);
+server.listen(process.env.PORT  || 3000);
 console.log('Server started');
