b5b06cd03acfa5ad246739b97c882edda3b2ee2d
leva <levlevlavrov@gmail.com>
2021-11-13 20:38:34 +0500

    server connected

--- server/main.js
+++ server/main.js
@@ -3,16 +3,16 @@
 const http = require("http");
 const fs = require('fs')
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(function (request, response) {
     console.log(request.method, request.url);
     if (request.url == '/style.css') {
-        const css = fs.readFileSync('../public/style.css', 'utf8');
+        const css = fs.readFileSync('public/style.css', 'utf8');
         response.end(css);
     } else {
-        const html = fs.readFileSync('../public/index.html', 'utf8');
+        const html = fs.readFileSync('public/index.html', 'utf8');
         response.end(html);
     }
 });
 
-server.listen(process.env.PORT  || 3000);
+server.listen(process.env.PORT || 3000);
 console.log('Server started');
